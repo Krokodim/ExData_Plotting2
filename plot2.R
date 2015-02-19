0e3beff633cbeaf64be6326cf17c8c3137bc3f93
Krokodim <dmitry.grekov@gmail.com>
2015-02-19 22:13:50 +0300

    sd

--- plot2.R
+++ plot2.R
@@ -4,12 +4,13 @@
 #     system to make a plot answering this question.                      #
 ###########################################################################
 
+
 library(dplyr)
 
 # read the data if necessary
 if (!exists("NEI")) NEI <- readRDS("summarySCC_PM25.rds")
 
-# filter and aggregate the data
+# filter and aggregate the dataw
   data <- NEI %>% filter(fips=="24510") %>% group_by(year) %>%  summarise(sum(Emissions)) 
 
 # set columns names for convenience
